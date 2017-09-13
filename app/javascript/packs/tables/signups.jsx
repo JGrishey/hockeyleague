e3e693ab09f340a37ac7fb78e33ea34b08c702d0
Jacob Grishey <jgrishey@gmail.com>
2017-09-12 23:14:36 -0400

    signups table

--- app/javascript/packs/tables/signups.jsx
+++ app/javascript/packs/tables/signups.jsx
@@ -16,6 +16,8 @@ class Signups extends React.Component {
     }
     render () {
         const { data } = this.state
+        const docWidth = document.getElementById("signups").offsetWidth - (1.25 * 16 * 2)
+        const actualWidth = document.body.clientWidth
         return (
             <ReactTable
                 data={data}
@@ -24,38 +26,44 @@ class Signups extends React.Component {
                         Header: "Name",
                         id: "name",
                         accessor: d => (<div><a href={"/" + d.name}>{d.name}</a></div>),
+                        minWidth: actualWidth >= 992 ? docWidth / (100 / 15) : 150
                     },
                     {
                         Header: "POS",
                         accessor: "preferred",
-                        style: {"textAlign": "center"}
+                        style: {"textAlign": "center"},
+                        maxWidth: actualWidth >= 992 ? docWidth / (100 / (70 / 4)) : 100
                     },
                     {
                         Header: "Willing",
                         id: "willing",
                         accessor: d => (d["willing"].join(", ")),
-                        style: {"textAlign": "center"}
+                        style: {"textAlign": "center"},
+                        minWidth: actualWidth >= 992 ? docWidth / (100 / 15) : 150
                     },
                     {
                         Header: "MIA",
                         accessor: "mia",
-                        style: {"textAlign": "center"}
+                        style: {"textAlign": "center"},
+                        maxWidth: actualWidth >= 992 ? docWidth / (100 / (70 / 4)) : 100
                     },
                     {
                         Header: "Veteran",
                         id: "veteran",
                         accessor: d => d.veteran ? "Yes" : "No",
-                        style: {"textAlign": "center"}
+                        style: {"textAlign": "center"},
+                        maxWidth: actualWidth >= 992 ? docWidth / (100 / (70 / 4)) : 100
                     },
                     {
                         Header: "Part-time",
                         id: "part_time",
                         accessor: d => d.part_time ? "Yes" : "No",
-                        style: {"textAlign": "center"}
+                        style: {"textAlign": "center"},
+                        maxWidth: actualWidth >= 992 ? docWidth / (100 / (70 / 4)) : 100
                     },
                 ]}
                 className="-striped -highlight"
-                resizable={false}
+                resizable={true}
                 defaultSorted={[
                     {
                         id: "name",
