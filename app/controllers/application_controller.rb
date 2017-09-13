3f20f27c1408738fbf6a7ffe70c5ad8f567ef9c4
Jacob Grishey <jgrishey@gmail.com>
2017-09-13 12:44:28 -0400

    prepend

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,5 @@
 class ApplicationController < ActionController::Base
-    protect_from_forgery with: :exception
+    protect_from_forgery with: :exception, prepend: true
     before_action :user_activity
     before_action :block_ip_addresses
 
