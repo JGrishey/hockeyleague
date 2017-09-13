4e71466360141ac6399357b91c96cca1e897e9c3
Jacob Grishey <jgrishey@gmail.com>
2017-09-13 13:28:42 -0400

    ssl

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -25,6 +25,8 @@ Rails.application.configure do
   # Do not fallback to assets pipeline if a precompiled asset is missed.
   config.assets.compile = false
 
+  config.force_ssl = true
+
   config.action_cable.allowed_request_origins = ['https://moosehockeyleague.herokuapp.com',
   'http://moosehockeyleague.herokuapp.com']
 
