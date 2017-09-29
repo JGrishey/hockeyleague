2ae13853c2806e4c5f37a0f276d068fdb8894b2c
Jacob Grishey <jgrishey@gmail.com>
2017-09-28 20:20:22 -0400

    sidebar

---
+++ app/assets/javascripts/sidebar.js
@@ -0,0 +1,7 @@
+function openNavigation() {
+    document.getElementById("sideNavigation").style.width = "100%"
+}
+
+function closeNavigation() {
+    document.getElementById("sideNavigation").style.width = "0%"
+}
