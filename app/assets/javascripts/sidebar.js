625febf838030fcf666dc69d402f709d9e7cc849
Jacob Grishey <jgrishey@gmail.com>
2017-11-06 19:12:44 -0500

    remove sidebar

--- app/assets/javascripts/sidebar.js
+++
@@ -1,7 +0,0 @@
-function openNavigation() {
-    document.getElementById("sideNavigation").style.width = "100%"
-}
-
-function closeNavigation() {
-    document.getElementById("sideNavigation").style.width = "0%"
-}
